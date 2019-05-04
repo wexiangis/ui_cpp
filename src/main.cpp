2d5f4fa662cd0b5d463063c0a9dbd751a79e0291
WWX <593805475@qq.com>
2019-05-04 19:55:12 +0800

    临时

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "viewPen.h"
 #include "viewPicture.h"
 #include "viewShape.h"
+#include "viewType.h"
 #include <iostream>
 
 int clock_count(int tag)
@@ -18,9 +19,19 @@ int clock_count(int tag)
     return result;
 }
 
-#define TEST_MODE 3
+#define TEST_MODE 4
 
-#if(TEST_MODE == 3)
+#if(TEST_MODE == 4)
+
+//
+int main(int argc, char **argv)
+{
+
+
+    return 0;
+}
+
+#elif(TEST_MODE == 3)
 
 #include <stdlib.h>
 #include <string.h>
@@ -162,27 +173,27 @@ int main(int argc, char **argv)
 
     while(1)
     {
-        clock_count();
+        clock_count(0);
         std::cout<<"\n";
 
         pen.print_rgb(res, 0, 0, resW, resH, 0);
-        clock_count();
+        clock_count(1);
         pen.print_map(res2, 50, 50, resW, resH, 0);
-        clock_count();
+        clock_count(2);
         pen.print_grid(grid, 0xFF0000, 100, 100, resW, resH, 0);
-        clock_count();
+        clock_count(3);
         pen.print_grid2(grid, 0xFF0000, 150, 150, resW, resH, 0);
-        clock_count();
+        clock_count(4);
         std::cout<<"\n";
 
         pen.print_rgb(res, 0, 0+200, resW, resH, 0.5);
-        clock_count();
+        clock_count(5);
         pen.print_map(res2, 50, 50+200, resW, resH, 0.5);
-        clock_count();
+        clock_count(6);
         pen.print_grid(grid, 0x0000FF, 100, 100+200, resW, resH, 0.5);
-        clock_count();
+        clock_count(7);
         pen.print_grid2(grid, 0x0000FF, 150, 150+200, resW, resH, 0.5);
-        clock_count();
+        clock_count(8);
         std::cout<<"\n";
 
         // pen.print_rgb(res, 0, 0, resW, resH, 0);
@@ -191,31 +202,6 @@ int main(int argc, char **argv)
         pen.output();
         sleep(1);
     }
-
-    return 0;
-}
-
-#elif(TEST_MODE == 0)
-//
-int main(int argc, char **argv)
-{
-
-    AnyType<string> vt13("vt13", new string[5]{"cdf", "234", "jjj", "kkk", "ddd"}, 5);
-
-    cout<<"class name: "<<vt13.name()<<endl;
-
-    cout<<vt13.get()<<", "<<vt13.get(1)<<endl;
-
-    string *ret = vt13.getAll();
-    cout<<ret[0]<<", "<<ret[1]<<", "<<ret[2]<<endl;
-    delete[] ret;
-
-    vt13.del(2);
-    vt13.insert("bbb", 2);
-
-    string *ret2 = vt13.getN(1, 3);
-    cout<<ret2[0]<<", "<<ret2[1]<<", "<<ret2[2]<<endl;
-    delete[] ret2;
 
     return 0;
 }
